52f2f2648f9328f054d43f894a510af54fc21b44
Kyle Leonard <kdleonard@icloud.com>
2020-04-14 19:33:26 -0500

    validatPost and started on userRouter

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,32 +1,66 @@
-const express = require('express');
+const express = require("express");
+const logger = require("../middleware/logger.js");
+const validateUserId = require("../middleware/validateUserId");
+const validateUser = require("../middleware/validateUser");
+const validatePost = require("../middleware/validatePost");
+
+const Users = require("./userDb");
+const Posts = require("../posts/postDb");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
+  // do your magic!
+  Users.insert(req.body)
+    .then((user) => {
+      res.status(201).json(user);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Error creating user",
+      });
+    });
+});
+
+router.post("/:id/posts", (req, res) => {
+  // do your magic!
+  req.body.user_id = req.user.id;
+  Posts.insert(req.body)
+    .then((posted) => {
+      Posts.getById(posted.id)
+        .then((post) => {
+          res.status(201).json(post);
+        })
+        .catch((err) => {
+          res.status(500).json({
+            errorMessage: "cannot retrieve newly made post",
+          });
+        });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        errorMessage: "Error creating new post",
+      });
+    });
+});
+
+router.get("/", (req, res) => {
   // do your magic!
 });
 
-router.post('/:id/posts', (req, res) => {
+router.get("/:id", (req, res) => {
   // do your magic!
 });
 
-router.get('/', (req, res) => {
+router.get("/:id/posts", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
-});
-
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // do your magic!
 });
 
