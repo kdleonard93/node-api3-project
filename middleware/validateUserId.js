8b7622843176ee94634731fb8bd4a33ca6c76ed8
Kyle Leonard <kdleonard@icloud.com>
2020-04-14 19:30:02 -0500

    Made corrections

--- middleware/validateUserId.js
+++ middleware/validateUserId.js
@@ -1,13 +1,15 @@
-module.exports = function validateUser(req, res, next) {
-  if (Object.keys(req.body).length === 0) {
-    res.status(400).json({
-      message: "missing user data",
-    });
-  } else if (!req.body.name) {
-    res.status(400).json({
-      message: "missing required text field",
-    });
-  } else {
-    next();
-  }
+const Users = require("../users/userDb");
+
+module.exports = function validateUserId(req, res, next) {
+  const { id } = req.params;
+  Users.getById(id).then((user) => {
+    if (user) {
+      req.user = user;
+      next();
+    } else if (!user) {
+      res.status(404).json({
+        message: "could not find user with that ID",
+      });
+    }
+  });
 };
