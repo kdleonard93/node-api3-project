3e6da7c0e8dd4ca6ff8236ebbac67c314c692df8
Kyle Leonard <kdleonard@icloud.com>
2020-08-13 19:07:33 -0500

    Got my index file set

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code away!
+const express = require("express")
+const logger = require("./middleware/logger")
+const userRouter = require("./users/userRouter")
+
+const server = express()
+
+const port = 4000;
+
+server.use(express.json())
+
+server.use(logger())
+
+server.use(userRouter)
+
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`)
+})
